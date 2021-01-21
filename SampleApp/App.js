9ed97af16d4b7db224fd717cf373fd78ee099de5
parkjaehoon <yukihoon12@naver.com>
2021-01-21 14:49:22 +0900

    d-day앱 만들기 1차

--- SampleApp/App.js
+++ SampleApp/App.js
@@ -1,114 +1,144 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  ImageBackground,
 } from 'react-native';
+import Setting from './Setting';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [dday, setDday] = useState(new Date());
+  const [ddayTitle, setDdayTitle] = useState('new Title');
+  const [chatLog, setChatLog] = useState([]);
+  const [settingModal, setSettingModal]  = useState(false);
 
-const App: () => React$Node = () => {
+  const modalHandler = () => {
+    setSettingModal(prev=>!prev)
+  }
+  const settingHandler = (title, date) => {
+    setDdayTitle(title);
+    setDday(date);
+    modalHandler();
+  }
+  const makeDateString = ()=>{
+    return dday.getFullYear() + '년 ' + (dday.getMonth()+1) + '월 ' + dday.getDate() + '일';
+  }
+  const  makeRemainString=()=>{
+    const distance = new Date().getTime() - dday.getTime();
+    console.log(new Date(), dday,distance / (1000 * 60 * 60 * 24) )
+    const remain = Math.floor(distance / (1000 * 60 * 60 * 24));
+    if(remain < 0) {
+      return 'D'+remain;
+    } else if (remain > 0) {
+      return 'D+'+remain;
+    } else if (remain === 0) {
+      return 'D-day';
+    }
+  }
+  
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <View style={styles.container}>
+      <ImageBackground
+        style={{width: '100%', height: '100%'}}
+        source={require('./icon/background.png')}>
+        <View style={styles.settingView}>
+          <TouchableOpacity onPress={()=>modalHandler()}>
+            <Image source={require('./icon/setting.png')} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.ddayView}>
+          <Text style={styles.titleText}>{ddayTitle}까지</Text>
+          <Text style={styles.ddayText}>{makeRemainString()}</Text>
+          <Text style={styles.dateText}>{makeDateString()}</Text>
+        </View>
+        <View style={styles.chatView}>
+          <ScrollView style={styles.chatScrollView}></ScrollView>
+          <View style={styles.chatControl}>
+            <TextInput style={styles.chatInput} />
+            <TouchableOpacity style={styles.sendButton}>
+              <Text>전송</Text>
+            </TouchableOpacity>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        </View>
+        {settingModal ? <Setting modalHandler={modalHandler} settingHandler={settingHandler}/> : <></>}
+      </ImageBackground>
+    </View>
   );
 };
-
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  settingView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    marginRight: '1%',
   },
-  body: {
-    backgroundColor: Colors.white,
+  ddayView: {
+    flex: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  chatView: {
+    flex: 6,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  titleText: {
+    alignSelf: 'flex-end',
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#4A4A4A',
+    marginRight: '15%',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  ddayText: {
+    fontSize: 100,
+    fontWeight: 'bold',
+    color: '#4A4A4A',
   },
-  highlight: {
-    fontWeight: '700',
+  dateText: {
+    alignSelf: 'flex-start',
+    fontSize: 21,
+    fontWeight: 'bold',
+    color: '#4A4A4A',
+    marginLeft: '15%',
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  sendButton: {
+    backgroundColor: 'rgb(97,99,250)',
+    height: 40,
+    width: 50,
+    borderRadius: 20,
+    padding: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+  },
+  chatInput: {
+    backgroundColor: 'white',
+    width: '75%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#a5a5a5',
+    borderRadius: 20,
+  },
+  chatControl: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  chatScrollView: {
+    width: '90%',
+    alignSelf: 'center',
+    backgroundColor: 'rgba(201,201,201,0.7)',
+    borderRadius: 5,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#a5a5a5',
   },
 });
-
 export default App;
---
+++ SampleApp/Setting.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+
+const Setting = ({modalHandler, settingHandler}) => {
+  const [title, setTitle] = useState('제목');
+  const [date, setDate] = useState(new Date());
+  ``;
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.background} onPress={modalHandler} activeOpacity={1}/>
+      <View style={styles.modal}>
+        <Text style={styles.titleText}>설정</Text>
+        <TextInput
+          style={styles.ddayInput}
+          value={title}
+          onChangeText={(changedText) => {setTitle(changedText)}}
+          placeholder={"디데이 제목을 입력해주세요."}
+        />
+        <DatePicker date={date} mode="date" onDateChange={date=>setDate(date)}/>
+        <TouchableOpacity onPress={()=>settingHandler(title,date)}>
+          <Text style={styles.doneText}>완료</Text>
+        </TouchableOpacity>
+      </View>
+      <Text>설정창</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'transparent',
+  },
+  background: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  ddayInput: {
+    backgroundColor: 'white',
+    marginBottom: 20,
+    width: '75%',
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#a5a5a5',
+  },
+  modal: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: '50%',
+    backgroundColor: 'white',
+  },
+  doneText: {
+    color: 'rgb(1,123,255)',
+    fontSize: 15,
+    margin: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    margin: 10,
+  },
+});
+export default Setting;
